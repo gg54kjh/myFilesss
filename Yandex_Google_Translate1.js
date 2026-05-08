6059687b6037617f3df185b89c57652aabe1f5fe
gg54kjh <73756720+gg54kjh@users.noreply.github.com>
2026-05-08 16:22:58 +0300

    Update and rename Yandex_Google_Translate.js to Yandex_Google_Translate1.js

--- Yandex_Google_Translate1.js
+++ Yandex_Google_Translate1.js
@@ -22,7 +22,7 @@ function Google_Translate(e) {
         method: "GET",
         url: url,
         onload: function(response) {
-           let ip = response.responseText; console.lo
+           let ip = JSON.parse(response.responseText);
            var arr=[], dict=[], perev2; 
          if (ip[1]) {
             for(i=0; i<ip[1].length; i++) 
